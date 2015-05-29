a978fef378021c61c7df88ae0a36fa96f682b6a5
John <jayelwhy@yahoo.com>
2015-05-29 10:43:35 -0700

    all functions of word_puzzle method work. Splitting of words, splitting of letters, replacing vowels and rejoining the words back together

--- lib/word_puzzle.rb
+++ lib/word_puzzle.rb
@@ -1,11 +1,18 @@
 class String
+
   define_method(:word_puzzle) do
     vowels = ["a", "e", "i", "o", "u"]
-    letters = self.split("")
-    letters.each do |letter|
-      if vowels.include?(letter)
-         letter.replace("-")
+    words = self.split(" ")
+    words.each do |word|
+      letters = word.split("")
+      letters.each do |letter|
+        if vowels.include?(letter)
+          letter.replace("-")
+        end
+      new_word = letters.join()
+      word.replace(new_word)
       end
     end
+    words.join(" ")
   end
 end
--- spec/word_freq_spec.rb
+++ spec/word_freq_spec.rb
@@ -6,11 +6,16 @@ require "./app"
 Capybara.app = Sinatra::Application
 
 describe('String#word_puzzle') do
+  it("split words in a sentence into separate strings with separate letters") do
+     expect(("the the").word_puzzle()).to(eq("th-th-"))
+  end
+
   it("takes the users string and splits it into individual letters") do
-    expect(("th").word_puzzle()).to(eq(["t", "h"]))
+    expect(("th").word_puzzle()).to(eq("th"))
   end
 
   it("replaces any vowels with '-'") do
-    expect(("the").word_puzzle()).to(eq(["t", "h", "-"]))
+    expect(("the").word_puzzle()).to(eq("th-"))
   end
+
 end
