038516a4688d767e204b033f9ae75ff9bfbd0489
John <jayelwhy@yahoo.com>
2015-05-29 10:00:56 -0700

    replace vowels in a single word with '-'.

--- lib/word_puzzle.rb
+++ lib/word_puzzle.rb
@@ -1,5 +1,11 @@
 class String
   define_method(:word_puzzle) do
+    vowels = ["a", "e", "i", "o", "u"]
     letters = self.split("")
+    letters.each do |letter|
+      if vowels.include?(letter)
+         letter.replace("-")
+      end
+    end
   end
 end
--- spec/word_freq_spec.rb
+++ spec/word_freq_spec.rb
@@ -7,6 +7,10 @@ Capybara.app = Sinatra::Application
 
 describe('String#word_puzzle') do
   it("takes the users string and splits it into individual letters") do
-    expect(("the").word_puzzle()).to(eq(["t", "h", "e"]))
+    expect(("th").word_puzzle()).to(eq(["t", "h"]))
+  end
+
+  it("replaces any vowels with '-'") do
+    expect(("the").word_puzzle()).to(eq(["t", "h", "-"]))
   end
 end
